7633c4ba31497172c21bc43dfe91235bdb71e40e
Dana Ward <Dana Ward>
2017-03-08 23:58:19 -0500

    forgotten export

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   random: require("./strategy/random"),
   noprincess: require("./strategy/noprincess"),
   goodguess: require("./strategy/goodguess"),
-  playerapi: require('./playerapi/playerapi')
+  playerapi: require('./playerapi/playerapi'),
+  cardtypes: require('./definitions/cardtypes')
 };
 
